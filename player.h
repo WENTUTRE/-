09a9271293db17b81103421d1ae169ebb79302c2
A8250220 <58546778+Bear00088@users.noreply.github.com>
2020-06-13 15:28:13 +0800

    Update and rename player to player.h

---
+++ player.h
@@ -0,0 +1,29 @@
+#ifndef player_H
+#define player_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class player
+{
+private:
+	string name;
+	double point;
+	vector<int> deck;
+	int amount;
+
+public:
+	player();
+	player(string);
+	void setName(string);
+	void setPoint(double);
+	void setCard(int);
+	string getName();
+	double getPoint();
+	void MyCard();
+	//string action();
+	void compare(player);
+	void display();
+};
+#endif
