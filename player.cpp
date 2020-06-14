6a607c3c3f35affa8dabef96e676b026a8221e0e
駱冠宇 <58327506+WENTUTRE@users.noreply.github.com>
2020-06-14 15:56:18 +0800

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,4 +1,5 @@
 #include "player.h"
+
 #include <iostream>
 using namespace std;
 
@@ -10,12 +11,13 @@ player::player()
 	amount = 0;
 }
 
-player::player(string name)
+player::player(string name)     
 {
 	this->name = name;
 	point = 0;
 	vector<int> deck(0);
 	amount = 0;
+	
 }
 
 void player::setName(string name)
@@ -23,79 +25,34 @@ void player::setName(string name)
 	this->name = name;
 }
 
-void player::setPoint(double point)
-{
-	this->point = point;
-}
 
-void player::setCard(int value)
-{
-	deck.push_back(value);
-	amount++;
-}
 
-string player::getName()
+
+
+string player::getName()    //得到名字
 {
 	return name;
 }
 
-double player::getPoint()
+
+bool player::action()      //是否抽排
 {
-	double total = 0;
-	double point[13] = { 1,2,3,4,5,6,7,8,9,10,0.5,0.5,0.5 };
-	for (int i = 0; i < amount; i++)
-	{
-		total += point[deck[i] % 13];
-	}
-
-	setPoint(total);
-
-	return total;
-}
-
-void player::getCard()
-{
-	string suits[4] = { "黑桃", "方塊", "菱形", "梅花" };
-	string ranks[13] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-	cout << "莊家:" << endl;
-	cout << suits[deck[amount-1] / 13] << ranks[deck[amount-1] % 13] << endl;
-}
-
-void player::MyCard()
-{
-	string suits[4] = { "黑桃", "方塊", "菱形", "梅花" };
-	string ranks[13] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-	for (int i = 0; i < amount; i++)
-		cout << suits[deck[i] / 13] << ranks[deck[i] % 13] << endl;
-	
-	cout << endl;
-}
-
-int player::getAmount()
-{
-	return amount;
-}
-
-bool player::action()
-{
-	cout << "第" << amount + 1 <<"次,是否加牌? Y/N";
-
 	string a;
-
+    cout << "第" << amount + 1 << "次,是否加牌? Y/N......." ;
 	cin >> a;
-
+	cout << "---------------------------" << endl;
+	amount+=1;
+	
 	if (a == "Y")
 		return 1;
 	else if (a == "N")
 		return 0;
 	else
 		cout << "try again." << endl;
-		return action();
+	return action();
 }
 
-bool player::overflow()
+bool player::overflow()    //是否爆
 {
 	if (getPoint() > 10.5)
 		return 1;
@@ -103,7 +60,7 @@ bool player::overflow()
 		return 0;
 }
 
-void player::compare(player player2)
+void player::compare(player player2)   //比大小
 {
 	if (getAmount() == player2.getAmount())
 	{
@@ -119,14 +76,14 @@ void player::compare(player player2)
 		player2.display();
 	}
 	else
-	{ 
+	{
 		cout << getName() << "win." << endl;
 		display();
 		player2.display();
 	}
 }
 
-void player::display()
+void player::display()   
 {
 	cout << getName() << ":" << endl;
 	cout << getPoint() << endl;
