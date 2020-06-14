daa12a849e5e87be73437f64a9070aa8f6b1cf83
A8250220 <58546778+Bear00088@users.noreply.github.com>
2020-06-14 20:43:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,55 +6,65 @@
 #include "Card.h"
 using namespace std;
 
-
-
-
-
 int main()
 {
-	vector<int> deck(52);
+	vector<int> deck(52); //公用牌組
 	string name;
 
 	for (int i = 0; i < 52; i++)
 		deck[i] = i;
 
-
+	//詢問名字
 	cout << "Your player name is?......." ;
 	cin >> name;
+
+	//創建人物
 	player p1(name);
 	player p2("莊家");
-	p1.drawcard(deck);
-	p2.setCard(p1._Card(deck));
-	p1.setCard(p1._Card(deck));
+
+	//洗牌
+	p1.shuffle(deck);
+
+	//第一輪拿牌
+	p2.setCard(p1.drawCard(deck));
+	p1.setCard(p1.drawCard(deck));
 	p1.display();
-	for (int i = 0; i < 4; i++)//second~
+
+	//第二輪之後
+	for (int i = 0; i < 4; i++)
 	{
-		p2.setCard(p1._Card(deck));
+		p2.setCard(p1.drawCard(deck));
 		p2.getPoint();
 		p2.getCard();
 
+		//詢問是否加牌
 		if (p1.action())
 		{
-			p1.setCard(p1._Card(deck));
+			p1.setCard(p1.drawCard(deck));
 			p1.display();
 		}
-		else continue;
 
+		//確認是否爆牌
 		if (p1.overflow())
 			break;
 		if (p2.overflow())
 			break;
 	}
+
+	//結果
 	if (p1.overflow())
 	{
+		cout << "玩家爆牌" << endl;
 		cout << p1.getName() << " lose." << endl;
 		p2.display();
 	}
 	else if (p2.overflow())
 	{
+		cout << "莊家爆牌" << endl;
 		cout << p2.getName() << " lose." << endl;
 		p2.display();
 	}
+	//若無爆牌,則比大小
 	else p1.compare(p2);
 
 	system("pause");
